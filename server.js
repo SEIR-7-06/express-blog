c8653ad7118f8434379a0647e70cec4395744767
kbbushman <k.bushman@yahoo.com>
2021-01-12 13:20:16 -0800

    Adds authorsController

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const authorsController = require('./controllers/authorsController');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 
 //------------------------ ROUTES
+
+// Home Page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/authors', (req, res) => {
-  res.render('authors/authorsIndex');
-});
+app.use('/authors', authorsController);
 
 // Start Server Listener
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
