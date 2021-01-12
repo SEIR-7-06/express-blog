ec470dd8772e0903c5d4cd262356ed70d8bdedd1
kbbushman <k.bushman@yahoo.com>
2021-01-12 15:26:23 -0800

    Updates demo code

--- controllers/authorsController.js
+++ controllers/authorsController.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 // CURRENT PATH === '/authors'
 
 
 // GET all Authors (Index)
 router.get('/', (req, res) => {
-  res.render('authors/authorsIndex');
+  // Get All Authors in DB
+  db.Author.find({}, (err, allAuthors) => {
+    if (err) {
+      console.log(err);
+      return res.send(err);
+    }
+    const context = {
+      authorsData: allAuthors,
+    };
+
+    res.render('authors/authorsIndex', context);
+
+  });
 });
 
 
@@ -18,6 +31,15 @@ router.get('/new', (req, res) => {
 // POST New Author From New Author Form
 router.post('/', (req, res) => {
   console.log(req.body);
+  // Create a new Author Object in MongoDB
+  db.Author.create(req.body, (err, newAuthor) => {
+    if (err) {
+      console.log(err);
+      return res.send(err);
+    }
+
+    res.redirect('/authors');
+  });
 });
 
 module.exports = router;
