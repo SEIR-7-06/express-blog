0d56b145094e2302191894fe52ccbcd4ac377e81
kbbushman <k.bushman@yahoo.com>
2021-01-12 12:04:21 -0800

    Adds ejs and authors index

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.set('view engine', 'ejs');
+
+//------------------------ ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/authors', (req, res) => {
+  res.render('authors/authorsIndex');
+});
 
 // Start Server Listener
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
